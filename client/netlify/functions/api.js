47cc128d15df19ed2cc964d3fc6028795786e43c
Nicolas Pomares <nicolaspomaresdev@gmail.com>
2023-10-11 18:30:02 +0900

    putting server code in netlify function

--- client/netlify/functions/api.js
+++ client/netlify/functions/api.js
@@ -1,10 +1,35 @@
+import OpenAI from "openai";
+
 exports.handler = async function (event, context) {
   // your server-side functionality
+  const { text } = event.queryStringParameters;
+
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  async function openaiApi(text) {
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Is ${text} spicy ? Answer by 'yes' or 'no' with no punctuation`,
+        },
+      ],
+      temperature: 0.1,
+      max_tokens: 1,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    const answer = response.choices[0].message.content;
+    console.log(answer);
+    return answer;
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
-    }),
+    body: await openaiApi(text),
   };
 };
