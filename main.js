64dfbf0bc42a4b7b84b384a3c016cfb510f388e6
Nicolas Pomares <nicolaspomaresdev@gmail.com>
2023-10-16 19:04:35 +0900

    trying different import netlifyidentity

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import netlifyIdentity from "netlify-identity-widget";
+import netlifyIdentity from "./node_modules/netlify-identity-widget";
 
 const form = document.querySelector("form");
 const textInput = document.querySelector(".textInput");
