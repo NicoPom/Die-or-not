42722c348f553053195b27e190c32fbaea237399
Nicolas Pomares <nicolaspomaresdev@gmail.com>
2023-10-25 19:54:05 +0900

    cleaned database.js

--- database.js
+++ database.js
@@ -3,101 +3,58 @@ import mysql from "mysql2";
 const pool = mysql.createPool(process.env.DB_URL_DEV).promise();
 
 const getUsers = async () => {
-  try {
-    const [result] = await pool.query("SELECT * FROM users");
-    return result;
-  } catch (err) {
-    console.error(err);
-  }
+  const [result] = await pool.query("SELECT * FROM users");
+  return result;
+};
+
+const getUserById = async (id) => {
+  const [result] = await pool.query("SELECT * FROM users WHERE id = ?", [id]);
+  return result;
 };
 
 const getUserByNetlifyId = async (id) => {
-  try {
-    const [result] = await pool.query(
-      "SELECT * FROM users WHERE netlify_id = ?",
-      [id]
-    );
-    return result;
-  } catch (err) {
-    console.error(err);
-  }
+  const [result] = await pool.query(
+    "SELECT * FROM users WHERE netlify_id = ?",
+    [id]
+  );
+  return result;
 };
 
 const getUserByStripeId = async (id) => {
-  try {
-    const [result] = await pool.query(
-      "SELECT * FROM users WHERE stripe_id = ?",
-      [id]
-    );
-    return result;
-  } catch (err) {
-    console.error(err);
-  }
+  const [result] = await pool.query("SELECT * FROM users WHERE stripe_id = ?", [
+    id,
+  ]);
+  return result;
 };
 
 const addApiCallCount = async (id) => {
-  try {
-    const [result] = await pool.query(
-      "UPDATE users SET api_calls = api_calls + 1 WHERE netlify_id = ?",
-      [id]
-    );
-    if (result.affectedRows !== 1) {
-      throw new Error("User not updated");
-    }
-    console.log("+1 api call count", await getUserByNetlifyId(id));
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const resetApiCallCount = async (id) => {
-  try {
-    const [result] = await pool.query(
-      "UPDATE users SET api_calls = 0 WHERE netlify_id = ?",
-      [id]
-    );
-    if (result.affectedRows !== 1) {
-      throw new Error("User not updated");
-    }
-    console.log("reset api call count", await getUserByNetlifyId(id));
-  } catch (err) {
-    console.error(err);
-  }
+  const [result] = await pool.query(
+    "UPDATE users SET api_calls = api_calls + 1 WHERE netlify_id = ?",
+    [id]
+  );
+  const [updatedUser] = await getUserByNetlifyId(id);
+  console.log("added 1 api count : ", updatedUser);
+  return updatedUser;
 };
 
 const updateUserRole = async (id, role) => {
-  try {
-    const [result] = await pool.query(
-      "UPDATE users SET role = ? WHERE netlify_id = ?",
-      [role, id]
-    );
-    if (result.affectedRows !== 1) {
-      throw new Error("User not updated");
-    }
-    console.log("updated user role", await getUserByNetlifyId(id));
-  } catch (err) {
-    console.error(err);
-  }
+  const [result] = await pool.query(
+    "UPDATE users SET role = ? WHERE netlify_id = ?",
+    [role, id]
+  );
+  const [updatedUser] = await getUserByNetlifyId(id);
+  console.log(updatedUser);
+  return updatedUser;
 };
 
 const createUser = async (user) => {
-  try {
-    const [result] = await pool.query(
-      "INSERT INTO users (name, email, role, netlify_id, stripe_id) VALUES (?, ?, ?, ?, ?)",
-      [user.name, user.email, user.role, user.netlify_id, user.stripe_id]
-    );
-
-    if (result.affectedRows !== 1) {
-      throw new Error("User not created");
-    }
-
-    const userId = result.insertId;
-    console.log("done");
-    return await getUserByNetlifyId(userId);
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+  const [result] = await pool.query(
+    "INSERT INTO users (name, email, role, netlify_id, stripe_id) VALUES (?, ?, ?, ?, ?)",
+    [user.name, user.email, user.role, user.netlify_id, user.stripe_id]
+  );
+  const [newUser] = await getUserById(result.insertId);
+  console.log(newUser);
+  return newUser;
 };
 
 export {
@@ -106,6 +63,5 @@ export {
   getUserByStripeId,
   createUser,
   addApiCallCount,
-  resetApiCallCount,
   updateUserRole,
 };
